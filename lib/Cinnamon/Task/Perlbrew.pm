d0e4f7e84ed1415d16801fd5c52e594e415d33c6
hayajo <hayajo@cpan.org>
2013-03-08 21:19:06 +0900

    Refactored

--- lib/Cinnamon/Task/Perlbrew.pm
+++ lib/Cinnamon/Task/Perlbrew.pm
@@ -16,17 +16,19 @@ our @EXPORT = qw(
     perlbrew_run
 );
 
-sub perlbrew_bin      { catfile( $_[0], qw/bin perlbrew/ ) }
-sub perlbrew_rc       { catfile( $_[0], qw/etc bashrc/ ) }
-sub perlbrew_perl_dir { catdir( $_[0], 'perls', $_[1] ) }
+sub _perlbrew_bin      { catfile( $_[0], qw/bin perlbrew/ ) }
+sub _perlbrew_rc       { catfile( $_[0], qw/etc bashrc/ ) }
+sub _perlbrew_perl_dir { catdir( $_[0], 'perls', $_[1] ) }
 
 sub perlbrew_run (&$$) {
     my $code          = shift;
-    my $perlbrew_root = shell_quote shift;
+    my $perlbrew_root = shift;
     my $perlbrew      = shell_quote shift;
 
-    my $perlbrew_bin = perlbrew_bin $perlbrew_root;
-    my $perlbrew_rc  = perlbrew_rc $perlbrew_root;
+    my $perlbrew_bin = shell_quote( _perlbrew_bin $perlbrew_root );
+    my $perlbrew_rc  = shell_quote( _perlbrew_rc $perlbrew_root );
+
+    $perlbrew_root   = shell_quote( $perlbrew_root );
 
     no strict 'refs';
     no warnings 'redefine';
@@ -40,18 +42,22 @@ sub perlbrew_run (&$$) {
             unless ( ref $_ eq 'Cinnamon::Remote' );
 
         my @cmd = @_;
-        my $pre_cmd = <<"EOS";
+
+        my $cmd_str = <<"EOS";
 export PERLBREW_ROOT=$perlbrew_root; \\
 export PERLBREW_HOME=$perlbrew_root; \\
-source $perlbrew_rc && \\
-perlbrew use $perlbrew && \\
+source $perlbrew_rc; \\
+perlbrew use $perlbrew; \\
 EOS
 
-        my $opt  = ( ref $cmd[0] eq 'HASH' ) ? shift @cmd : undef;
-        my @args = ( $pre_cmd . join( ' ', @cmd ) );
-        unshift @args, $opt if ($opt);
+        if ( ref $cmd[0] eq 'HASH' ) {
+            splice( @cmd, 1, 0, $cmd_str );
+        }
+        else {
+            unshift( @cmd, $cmd_str );
+        }
 
-        $orig_run->(@args);
+        $orig_run->(@cmd);
     };
 
     $code->();
@@ -60,13 +66,15 @@ EOS
 task perlbrew => {
     setup => sub {
         my ( $host, @args ) = @_;
-        my $perlbrew_root   = shell_quote( get 'perlbrew_root' );
-        my $perlbrew_sudo   = get 'perlbrew_sudo';
-        my $perlbrew_bin    = perlbrew_bin $perlbrew_root;
+        my $perlbrew_root   = get 'perlbrew_root';
+        my $perlbrew_sudo   = get 'perlbrew_sudo' || 0;
+        my $perlbrew_bin    = _perlbrew_bin $perlbrew_root;
+
+        $perlbrew_root = shell_quote($perlbrew_root);
 
         remote {
-            my $cmd = <<"EOS";
-export PERLBREW_ROOT=$perlbrew_root
+            _run( <<"EOS", $perlbrew_sudo );
+export PERLBREW_ROOT=$perlbrew_root; \\
 if [ ! -e $perlbrew_bin ]; then \\
   curl -kL http://install.perlbrew.pl > perlbrew-install; \\
   /bin/sh perlbrew-install; \\
@@ -77,85 +85,101 @@ else \\
   $perlbrew_bin -f install-cpanm; \\
 fi
 EOS
-            chomp $cmd;
-            _run( $cmd, $perlbrew_sudo );
         } $host;
     },
     perl => {
         install => sub {
             my ( $host, @args ) = @_;
-            my $perlbrew_root = shell_quote( get 'perlbrew_root' );
+            my $perlbrew_root = get 'perlbrew_root';
             my $version       = shell_quote( get 'perlbrew_perl_version' );
-            my $install_opts  = get 'perlbrew_perl_install_options';
-            my $perlbrew_sudo = get 'perlbrew_sudo';
-            my $perlbrew_bin  = perlbrew_bin $perlbrew_root;
+            my $install_opts  = get 'perlbrew_perl_install_options' || [];
+            my $perlbrew_sudo = get 'perlbrew_sudo' || 0;
+            my $perlbrew_bin  = shell_quote( _perlbrew_bin $perlbrew_root );
+
+            $perlbrew_root = shell_quote($perlbrew_root);
+
+            my $cmd_str = <<"EOS";
+export PERLBREW_ROOT=$perlbrew_root; \\
+$perlbrew_bin install --verbose
+EOS
+            chomp $cmd_str;
+            for (@$install_opts) {
+                $cmd_str .= ' ' . shell_quote($_);
+            }
+            $cmd_str .= " $version\n";
 
             remote {
-                _run( "export PERLBREW_ROOT=$perlbrew_root; $perlbrew_bin install --verbose $version $install_opts", $perlbrew_sudo );
+                _run( $cmd_str, $perlbrew_sudo );
             } $host;
         },
         uninstall => sub {
             my ( $host, @args ) = @_;
-            my $perlbrew_root = shell_quote( get 'perlbrew_root' );
+            my $perlbrew_root = get 'perlbrew_root';
             my $version       = shell_quote( get 'perlbrew_perl_version' );
-            my $perlbrew_sudo = get 'perlbrew_sudo';
-            my $perlbrew_bin  = perlbrew_bin $perlbrew_root;
+            my $perlbrew_sudo = get 'perlbrew_sudo' || 0;
+            my $perlbrew_bin  = shell_quote( _perlbrew_bin $perlbrew_root );
+
+            $perlbrew_root = shell_quote($perlbrew_root);
 
             remote {
-                _run( "export PERLBREW_ROOT=$perlbrew_root; $perlbrew_bin uninstall $version", $perlbrew_sudo );
+                _run( <<"EOS", $perlbrew_sudo );
+export PERLBREW_ROOT=$perlbrew_root; \\
+$perlbrew_bin uninstall $version
+EOS
             } $host;
         },
     },
     lib => {
         create => sub {
             my ( $host, @args ) = @_;
-            my $perlbrew_root = shell_quote( get 'perlbrew_root' );
+            my $perlbrew_root = get 'perlbrew_root';
             my $perlbrew      = shell_quote( get 'perlbrew' );
-            my $perlbrew_sudo = get 'perlbrew_sudo';
-            my $perlbrew_bin  = perlbrew_bin $perlbrew_root;
+            my $perlbrew_sudo = get 'perlbrew_sudo' || 0;
+            my $perlbrew_bin  = shell_quote( _perlbrew_bin $perlbrew_root );
 
             remote {
-                my $cmd = <<"EOS";
+                _run( <<"EOS", $perlbrew_sudo );
 export PERLBREW_ROOT=$perlbrew_root; \\
 export PERLBREW_HOME=$perlbrew_root; \\
 $perlbrew_bin lib create $perlbrew
 EOS
-                chomp $cmd;
-                _run( $cmd, $perlbrew_sudo );
             } $host;
         },
         delete => sub {
             my ( $host, @args ) = @_;
-            my $perlbrew_root = shell_quote( get 'perlbrew_root' );
+            my $perlbrew_root = get 'perlbrew_root';
             my $perlbrew      = shell_quote( get 'perlbrew' );
-            my $perlbrew_sudo = get 'perlbrew_sudo';
-            my $perlbrew_bin  = perlbrew_bin $perlbrew_root;
+            my $perlbrew_sudo = get 'perlbrew_sudo' || 0;
+            my $perlbrew_bin  = shell_quote( _perlbrew_bin $perlbrew_root );
 
             remote {
-                my $cmd = <<"EOS";
+                _run( <<"EOS", $perlbrew_sudo );
 export PERLBREW_ROOT=$perlbrew_root; \\
 export PERLBREW_HOME=$perlbrew_root; \\
 $perlbrew_bin lib delete $perlbrew
 EOS
-                chomp $cmd;
-                _run( $cmd, $perlbrew_sudo );
             } $host;
         },
     },
     cpanm => sub {
         my ( $host, @args ) = @_;
-        my $perlbrew_root   = shell_quote( get 'perlbrew_root' );
+        my $perlbrew_root   = get 'perlbrew_root';
         my $perlbrew        = shell_quote( get 'perlbrew' );
         my $modules         = get 'cpanm_modules' || [];
-        my $opts            = get 'cpanm_options';
-        my $perlbrew_sudo   = get 'perlbrew_sudo';
-        my $perlbrew_bin    = perlbrew_bin $perlbrew_root;
-        my $perlbrew_rc     = perlbrew_rc $perlbrew_root;
+        my $cpanm_opts      = get 'cpanm_options' || [];
+        my $perlbrew_sudo   = get 'perlbrew_sudo' || 0;
+        my $perlbrew_bin    = shell_quote( _perlbrew_bin $perlbrew_root );
+        my $perlbrew_rc     = shell_quote( _perlbrew_rc $perlbrew_root );
+
+        my $cmd_str = "cpanm";
+        for ( @$cpanm_opts, @$modules ) {
+            $cmd_str .= ' ' . shell_quote($_);
+        }
+        $cmd_str .= "\n";
 
         remote {
             perlbrew_run {
-                my $_modules = join( ' ', @$modules );
-                _run( "cpanm $opts $_modules", $perlbrew_sudo );
+                _run( $cmd_str, $perlbrew_sudo );
             } $perlbrew_root, $perlbrew;
         } $host;
     },
@@ -244,7 +268,7 @@ This is B<alpha> version.
 
 =over 4
 
-=item prelbrew_run ( I<$sub: CODE> I<$perlbrew_root: String> I<$perlbrew: String> ): Any
+=item perlbrew_run ( I<$sub: CODE> I<$perlbrew_root: String> I<$perlbrew: String> ): Any
 
 This is supported only under C<remote>.
 
